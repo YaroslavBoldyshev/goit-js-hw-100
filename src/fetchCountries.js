b7037a15233880ddef4be2be86e3447a9ccddfa7
YaroslavBoldyshev <110547217+YaroslavBoldyshev@users.noreply.github.com>
2022-11-30 14:48:33 +0200

    5

---
+++ src/fetchCountries.js
@@ -0,0 +1,6 @@
+function fetchCountries(input) {
+    return fetch(`https://restcountries.com/v3.1/name/${input}?fields=name,capital,population,flags,languages`)
+        .then(p => p.json()) 
+}
+
+export default fetchCountries ;
