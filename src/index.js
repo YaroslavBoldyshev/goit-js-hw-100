0b18f1ac99cfc0e24f300beb979295768add4785
YaroslavBoldyshev <110547217+YaroslavBoldyshev@users.noreply.github.com>
2022-12-01 17:01:26 +0200

    qwerrty

--- src/index.js
+++ src/index.js
@@ -47,20 +47,19 @@ function createCountriesMarkup(countryArr) {
     .join('');
 }
 function createCountryMarkup(country) {
-  // const lang = [];
-  // for (key in country.languages) {
-  //   lang.push(country.languages[key]);
-  // }
-  // console.log(lang);
+  const lang = [];
+  const languages = { ...country.languages };
+  for (key in languages) {
+    lang.push(languages[key]);
+  }
+  console.log(lang);
   return `
     <ul>
-    <li><img src='${country.flags.svg}' width='30' height='30'><h2>${
-    country.name.official
-  }</h2></li>
+    <li><img src='${country.flags.svg}' width='30' height='30'><h2>${country.name.official}</h2></li>
     <li>Capital: <span>${country.capital}</span></li>
     <li>Population: ${country.population}</li>
-    <li>Languages: ${lang.join(', ')}</li>
-    <li>Languages: ${country.languages}</li>
+   
+    <li>Languages: ${lang}</li>
     </ul>
     `;
 }
